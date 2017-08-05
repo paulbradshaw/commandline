3e7655392d2b8346560cfb6993a62276694d7cd4
Paul Bradshaw <paulonhismobile@gmail.com>
2017-08-05 18:59:47 +0100

    cp version of mv

---
+++ copyandcombinecsv.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "looking for street crime CSV files..."
+find $HOME -name "*street.csv"
+echo "making a new folder on the Desktop..."
+mkdir -p $HOME/Desktop/allcombined
+echo "moving all *street.csv files to new folder..."
+cp `find $HOME -name "*street.csv"` $HOME/Desktop/allcombined
+echo "moving to new directory..."
+cd $HOME/Desktop/allcombined
+echo "combining all the files into one"
+cat *street.csv > crimescombined.csv
