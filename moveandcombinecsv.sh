bc8debb3439c57121792940841d8f88a501797fd
Paul Bradshaw <paulonhismobile@gmail.com>
2017-08-05 18:33:24 +0100

    added -p

--- moveandcombinecsv.sh
+++ moveandcombinecsv.sh
@@ -3,6 +3,6 @@
 echo "looking for street crime CSV files..."
 find $HOME -name "*street.csv"
 echo "making a new folder on the Desktop..."
-mkdir $HOME/Desktop/allcombined
+mkdir -p $HOME/Desktop/allcombined
 echo "moving all *street.csv files to new folder..."
 mv `find $HOME -name "*street.csv"` $HOME/Desktop/allcombined
