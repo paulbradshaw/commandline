319470868e168abaded112272dbfffa08c7e224c
Paul Bradshaw <paulonhismobile@gmail.com>
2017-08-05 18:42:57 +0100

    added cat to shell

--- moveandcombinecsv.sh
+++ moveandcombinecsv.sh
@@ -6,3 +6,7 @@ echo "making a new folder on the Desktop..."
 mkdir -p $HOME/Desktop/allcombined
 echo "moving all *street.csv files to new folder..."
 mv `find $HOME -name "*street.csv"` $HOME/Desktop/allcombined
+echo "moving to new directory..."
+cd $HOME/Desktop/allcombined
+echo "combining all the files into one"
+cat *street.csv > crimescombined.csv
