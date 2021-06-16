020232a6f94aa23d6c56fd71270a07c81b82299a
Paul Bradshaw <paulonhismobile@gmail.com>
2021-06-16 13:43:04 +0100

    Update rtfconvertandgrep.sh

--- shellscripts/rtfconvertandgrep.sh
+++ shellscripts/rtfconvertandgrep.sh
@@ -7,7 +7,7 @@ mkdir txtfiles
 echo "moving txt files into new directory"
 mv *.txt txtfiles
 echo "grepping mentions of different words"
-grep "black" txtfiles/*.txt > mentionsblack.txt
-grep "white" txtfiles/*.txt > mentionswhite.txt
-grep "asian" txtfiles/*.txt > mentionsasian.txt
-grep "ietnamese" txtfiles/*.txt > mentionsviet.txt
+grep -i "black" txtfiles/*.txt > mentionsblack.txt
+grep -i "white" txtfiles/*.txt > mentionswhite.txt
+grep -i "asian" txtfiles/*.txt > mentionsasian.txt
+grep -i "vietnamese" txtfiles/*.txt > mentionsviet.txt
