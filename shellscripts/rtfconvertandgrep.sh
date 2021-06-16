4b26da60ad52ec373779d05f70df3346f782597d
Paul Bradshaw <paulonhismobile@gmail.com>
2021-06-16 13:46:45 +0100

    Update rtfconvertandgrep.sh

--- shellscripts/rtfconvertandgrep.sh
+++ shellscripts/rtfconvertandgrep.sh
@@ -9,5 +9,6 @@ mv *.txt txtfiles
 echo "grepping mentions of different words"
 grep -i "black" txtfiles/*.txt > mentionsblack.txt
 grep -i "white" txtfiles/*.txt > mentionswhite.txt
-grep -i "asian" txtfiles/*.txt > mentionsasian.txt
+grep -i "Asian" txtfiles/*.txt > mentionsasian.txt
 grep -i "vietnamese" txtfiles/*.txt > mentionsviet.txt
+grep -i "described as" txtfiles/*.txt > mentionsdesc.txt
