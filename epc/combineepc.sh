21b1777258b94905946a53a1e391ff44c0e9a225
Paul Bradshaw <paulonhismobile@gmail.com>
2023-02-08 10:52:32 +0000

    Update combineepc.sh

--- epc/combineepc.sh
+++ epc/combineepc.sh
@@ -5,6 +5,6 @@ cd all-domestic-certificates
 echo "looking for EPC CSV files in folders beginning ‘domestic’"
 find domestic* -name "certificates.csv"
 echo "copying, moving and renaming those files into the current directory"
-num=0; for i in `find output_dir -name "certificates.csv"`; do cp "$i" "$(printf '%03d' $num).${i#*.}"; ((num++)); done
+num=0; for i in `find domestic* -name "certificates.csv"`; do cp "$i" "$(printf '%03d' $num).${i#*.}"; ((num++)); done
 echo "combining all the files into one"
 cat *.csv > certificatescombined.csv
